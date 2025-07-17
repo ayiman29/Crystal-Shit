06da2a3afc570d651d32c7303b9d295829d341fe
RZ Ayiman <ayimaneg@gmail.com>
2025-07-17 19:43:53 +0600

    Initial Commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="logo-container">
+        <img src="../images/The_Doors_Logo.png" alt="globe icon" />
+      </div>
+      <h1>CRYSTAL SHIT - THE BEST COVER BAND</h1>
+    </header>
+  );
+}
---
+++ data.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "./images/jimmy.png",
+            alt: "Jim Morrison"
+        },
+        title: "Jimothy Morrison",
+        country: "Florida",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "08 Dec, 1943 – 03 Jul, 1971",
+        text: "The lead singer of the god-awful band Crystal Shit. He freakin' sucked at singing... like, actually awful. He wanted to be loved two times because, he got AIDS. Loved by who, you ask? Considering how many times he mentioned little girls in his songs, it’s honestly concerning. Luckily, he died in 1971 when his super-tight leather pants crushed his pelvic glands and strangled him to death."
+    },
+    {
+        id: 2,
+        img: {
+            src: "./images/reimann.png",
+            alt: "Ray Manzarek"
+        },
+        title: "Reimann Zarek",
+        country: "Illinois",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "12 Feb, 1939 – 20 May, 2013",
+        text: "This guy peaked when he played piano for Scott Weiland, a vocalist 100 times better than Jimothy."
+    },
+    {
+        id: 3,
+        img: {
+            src: "./images/robbie.png",
+            alt: "robbie"
+        },
+        title: "Robbie",
+        country: "California",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "08 Jan, 1946 – Present",
+        text: "Robbie was the left-hand guy for Jimothy. Since the Jimothy client list never dropped, we still don’t know what he was really up to, but Robbie was the one doing the dirty work for him. The command for Robbie to act was always: “Do it, Robbie. Do it!” which would trigger his MKUltra programming and make him obey whatever his master wanted. Oh, and yeah, he played guitar."
+    },
+    {
+        id: 4,
+        img: {
+            src: "./images/John Densmore.png",
+            alt: "John Densmore"
+        },
+        title: "Jonathan Densmore",
+        country: "California",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Dec, 1944 – Present",
+        text: "He is the fourth Shitles. Nobody likes him."
+    },
+]
